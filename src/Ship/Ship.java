43fd8964b53036a50179063094198b73f34c1749
hisaacdelr <13808272+hisaacdelr@users.noreply.github.com>
2022-12-14 00:52:03 -0800

    pass Spot[] to Ship constructor

--- src/Ship/Ship.java
+++ src/Ship/Ship.java
@@ -1,9 +1,12 @@
-package Ship;/*
+package Ship;
 
- */
+import Board.Spot;
 
 public class Ship {
-    public Ship(){
-
+    private Spot[] spots;
+    public Ship(Spot[] spots){
+        this.spots = spots;
     }
+
+
 }
