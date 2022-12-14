da497cd8c4410bed223b4b20d409b76839323756
hisaacdelr <13808272+hisaacdelr@users.noreply.github.com>
2022-12-14 01:02:47 -0800

    separate setup and game start

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,15 +1,30 @@
 package Game;
 
-
+import java.util.ArrayList;
 import Player.CpuPlayer;
 import Player.HumanPlayer;
 import Player.Player;
 
 
 public class Game {
-    public void startGame(){
-        System.out.println("Inside game object");
+
+    private ArrayList<Player> playerList = new ArrayList<>();
+    public void setup(){
+        System.out.println("Setting up players in the game...");
         Player humanPlayer = new HumanPlayer();
         Player cpuPlayer = new CpuPlayer();
+
+
+        playerList.add(humanPlayer);
+        playerList.add(cpuPlayer);
+    }
+
+    public void startGame(){
+        System.out.println("Starting game!");
+
+        for(Player player : playerList){
+            player.handleTurn();
+        }
+
     }
 }
