d2094282d05632a3ed80446d3c6a1ef90f68921d
hisaacdelr <13808272+hisaacdelr@users.noreply.github.com>
2022-12-14 01:22:37 -0800

    create setup methods for Game and Board

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -13,6 +13,26 @@ public class Board {
     public Board(){
         boardSize = BOARD_SIZE;
         grid = new Spot[boardSize][boardSize]; // [row][col]
+
+        setup();
+    }
+
+    // Move to utility function
+    private void setup(){
+        // initialize spot objects
+        for(int row = 0; row < BOARD_SIZE; row++){
+            for(int col = 0; col < BOARD_SIZE; col++){
+                grid[row][col] = new Spot(row, col);
+            }
+        }
+
+        int[] shipSizes = new int[]{2, 3, 4, 5};
+
+//      TEST: initialize size 2 ship at 0,0 and 0,1
+        // placeShipsOnBoard()
+        grid[0][0].setOccupied(true);
+        grid[0][1].setOccupied(true);
+
     }
 
     /*
